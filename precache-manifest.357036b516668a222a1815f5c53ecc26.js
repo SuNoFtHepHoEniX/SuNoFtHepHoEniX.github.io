18f17a2cf0ae18dd584300544f3f3730feddddd6
SuNoFtHepHoEniX <zumrudu.anka.oyun@gmail.com>
2020-03-31 19:08:37 +0300

    Updates

---
+++ precache-manifest.357036b516668a222a1815f5c53ecc26.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02fac188a1e42632a054d52fc8211a1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa3487d52bb7ff13aa4f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c2b02108dfa323aa8a4b",
+    "url": "/static/js/2.e8b7283b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e8b7283b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa3487d52bb7ff13aa4f",
+    "url": "/static/js/main.04eb4701.chunk.js"
+  },
+  {
+    "revision": "b4dcfde51a3341c1d67d",
+    "url": "/static/js/runtime-main.58623e5a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
